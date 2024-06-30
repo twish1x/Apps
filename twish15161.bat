a3a1e94433d19966a1b207724c9a5c7d9d9b1602
twish1x <153032092+twish1x@users.noreply.github.com>
2024-06-30 00:25:42 -0700

    Update twish15161.bat

--- twish15161.bat
+++ twish15161.bat
@@ -4,7 +4,7 @@ cls
 
 curl -g -k -L -# -o C:\optimizations\PowerRun.exe https://github.com/twish1x/Apps/raw/main/PowerRun.exe
 
-curl -g -k -L -# -o C:\optimizations\DevManView.exe https://cdn.discordapp.com/attachments/1238699753387069551/1256871404796641311/DevManView.exe?ex=66825836&is=668106b6&hm=e21a330986307e96c0c0b09e806b6600babf5e2cdbbbbf02574c2f87cc66d2cf&
+curl -g -k -L -# -o C:\optimizations\DevManView.exe https://github.com/twish1x/Apps/raw/main/DevManView.exe
 
 curl -g -k -L -# -o C:\optimizations\driverdisable.bat https://raw.githubusercontent.com/twish1x/Apps/main/driver.bat
 
